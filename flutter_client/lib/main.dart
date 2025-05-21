f82f4d9bc8eb9945f0aa98990013dc46eb73894b
yephonekyaw <yephonekyaw231202@gmail.com>
2025-05-21 21:45:00 +0700

    setup firebase proj settings

--- flutter_client/lib/main.dart
+++ flutter_client/lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_client/firebase_options.dart';
 import 'package:flutter_client/router/router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(ProviderScope(child: TrasholiniApp()));
 }
 
